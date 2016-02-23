50d74b64ae1cd5df8b9d5ca0e8f624450b197620
Jonathon Taylor <jonathont@berkeley.edu>
2016-02-23 15:41:51 -0800

    working new-servicenowincident

--- posh-servicenow.psm1
+++ posh-servicenow.psm1
@@ -2,17 +2,6 @@
     Ref:  http://wiki.servicenow.com/index.php?title=Table_API
 #>
 
-Add-Type -TypeDefinition @"
-   public enum HTTP_METHOD
-   {
-      GET,
-      PUT,
-      POST,
-      PATCH,
-      DELETE
-   }
-"@
-
 New-Variable -Name INCIDENT_URI -Value 'api/now/v1/table/incident' -Option Constant
 
 Function Invoke-TableApiRequest
@@ -20,23 +9,23 @@ Function Invoke-TableApiRequest
     Param(
         [Parameter(Mandatory=$True)][PSCredential]$credential,
         [Parameter(Mandatory=$True)][string]$uri,
-        [Parameter(Mandatory=$True)][HTTP_METHOD]$httpMethod,
+        [Parameter(Mandatory=$True)][ValidateSet('Put','Post','Get','Patch')][string]$httpMethod,
         [Parameter(Mandatory=$False)][hashtable]$requestHash
     )
 
     
     $headers = @{'Content-Type' = 'application/json';'Accept' = 'application/json'}
 
-    # Let's use the .Net JavaScript serializer, the default PowerShell implementation limits return size
+    # Use the .Net JavaScript serializer, the default PowerShell implementation limits return size
     [void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
     $javaScriptSerializer = New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer
     $javaScriptSerializer.MaxJsonLength = [int]::MaxValue
     $javaScriptSerializer.RecursionLimit = 99
 
-    # Make our request to the web service
+    # Make a request to the web service
     if($requestHash -ne $null)
     {
-        $requestBody =  @{data=ConvertTo-Json($requestHash) -Depth 10}
+        $requestBody =  ConvertTo-Json($requestHash) -Depth 10
         $resp = Invoke-WebRequest -Method $httpMethod -Uri $uri -Body $requestBody -TimeoutSec 480 -DisableKeepAlive:$True -UseBasicParsing -Headers $headers -Credential $credential
     }
     else
@@ -44,10 +33,10 @@ Function Invoke-TableApiRequest
         $resp = Invoke-WebRequest -Method $httpMethod -Uri $uri -TimeoutSec 480 -DisableKeepAlive:$True -UseBasicParsing -Headers $headers -Credential $credential
     }
 
-    # Deserialize the request into a PowerShell object
+    # Deserialize the content, it will either a Dictionary or array of Dictionary
     $result = $javaScriptSerializer.DeserializeObject($resp.Content)
 
-    # Looks like we have at least one object to return, let's create a PSObject because it's easier to enum
+    # Return PSObjects from the result for convenience
     foreach ($thisResult in $result.result)
     {
         $thisObject = New-Object -Type PSObject -Property $thisResult
@@ -78,7 +67,38 @@ Function Get-ServiceNowIncident
     $fullUri = "$uri$requestUri"
 
     # send the request
-    Invoke-TableApiRequest -credential $credential -uri $fullUri -httpMethod GET
+    Invoke-TableApiRequest -credential $credential -uri $fullUri -httpMethod Get
+}
+
+Function New-ServiceNowIncident
+{
+    Param(
+        [Parameter(Mandatory=$True)][PSCredential]$credential,
+        [Parameter(Mandatory=$True)][string]$uri,
+        [Parameter(Mandatory=$True)][string]$assignmentGroup,
+        [Parameter(Mandatory=$True)][string]$shortDescription,
+        [Parameter(Mandatory=$True)][string]$cmdbCI,
+        [Parameter(Mandatory=$True)][string]$category,
+        [Parameter(Mandatory=$True)][string]$comments,
+        [Parameter(Mandatory=$True)][string]$contactType
+    )
+
+    # Build the URI
+    $requestUri = "$INCIDENT_URI`?&sysparm_exclude_reference_link=true&sysparm_display_value=true"
+    $fullUri = "$uri$requestUri"
+
+    # Crease HashTable for request body (content of the incident)
+    $requestHash = @{
+        'assignment_group' = $assignmentGroup;
+        'cmdb_ci' = $cmdbCI;
+        'category' = $category;
+        'short_description' = $shortDescription;
+        'comments' = $comments;
+        'contact_type' = $contactType;
+        }
+
+    # Send the request
+    Invoke-TableApiRequest -credential $credential -uri $fullUri -httpMethod Post -requestHash $requestHash
 }
 
 Export-ModuleMember *

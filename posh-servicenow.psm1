b0c746d81c7b9394edb4ef8117698d1464c34e68
Jonathon Taylor <jonathont@berkeley.edu>
2016-02-23 16:38:11 -0800

    better cmdletbinding conformance

--- posh-servicenow.psm1
+++ posh-servicenow.psm1
@@ -15,36 +15,38 @@ Function Invoke-TableApiRequest
         [Parameter(Mandatory=$False)][hashtable]$requestHash
     )
 
-    
-    $headers = @{'Content-Type' = 'application/json';'Accept' = 'application/json'}
-
-    # Use the .Net JavaScript serializer, the default PowerShell implementation limits return size
-    [void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
-    $javaScriptSerializer = New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer
-    $javaScriptSerializer.MaxJsonLength = [int]::MaxValue
-    $javaScriptSerializer.RecursionLimit = 99
-
-    # Make a request to the web service
-    if($requestHash -ne $null)
+    PROCESS
     {
-        $requestBody =  ConvertTo-Json($requestHash) -Depth 10
-        $resp = Invoke-WebRequest -Method $httpMethod -Uri $uri -Body $requestBody -TimeoutSec 480 -DisableKeepAlive:$True -UseBasicParsing -Headers $headers -Credential $credential
-    }
-    else
-    {
-        $resp = Invoke-WebRequest -Method $httpMethod -Uri $uri -TimeoutSec 480 -DisableKeepAlive:$True -UseBasicParsing -Headers $headers -Credential $credential
-    }
+        $headers = @{'Content-Type' = 'application/json';'Accept' = 'application/json'}
 
-    # Deserialize the content, it will either a Dictionary or array of Dictionary
-    $result = $javaScriptSerializer.DeserializeObject($resp.Content)
+        # Use the .Net JavaScript serializer, the default PowerShell implementation limits return size
+        [void][System.Reflection.Assembly]::LoadWithPartialName("System.Web.Extensions")
+        $javaScriptSerializer = New-Object -TypeName System.Web.Script.Serialization.JavaScriptSerializer
+        $javaScriptSerializer.MaxJsonLength = [int]::MaxValue
+        $javaScriptSerializer.RecursionLimit = 99
 
-    # Return PSObjects from the result for convenience
-    foreach ($thisResult in $result.result)
-    {
-        $thisObject = New-Object -Type PSObject -Property $thisResult
+        # Make a request to the web service
+        if($requestHash -ne $null)
+        {
+            $requestBody =  ConvertTo-Json($requestHash) -Depth 10
+            $resp = Invoke-WebRequest -Method $httpMethod -Uri $uri -Body $requestBody -TimeoutSec 480 -DisableKeepAlive:$True -UseBasicParsing -Headers $headers -Credential $credential
+        }
+        else
+        {
+            $resp = Invoke-WebRequest -Method $httpMethod -Uri $uri -TimeoutSec 480 -DisableKeepAlive:$True -UseBasicParsing -Headers $headers -Credential $credential
+        }
+
+        # Deserialize the content, it will either a Dictionary or array of Dictionary
+        $result = $javaScriptSerializer.DeserializeObject($resp.Content)
+
+        # Return PSObjects from the result for convenience
+        foreach ($thisResult in $result.result)
+        {
+            $thisObject = New-Object -Type PSObject -Property $thisResult
         
-        # put the object on the pipline
-        $thisObject
+            # put the object on the pipline
+            $thisObject
+        }
     }
 }
 
@@ -57,20 +59,24 @@ Function Get-ServiceNowIncident
         [Parameter(Mandatory=$False)][string]$incidentNumber
     )
 
-    # build the requestUri
-    if(![String]::IsNullOrEmpty($incidentNumber))
+    PROCESS
     {
-        $requestUri = "$INCIDENT_URI`?&sysparm_exclude_reference_link=true&sysparm_display_value=true&sysparm_query=number=$($incidentNumber.Trim())"
-    }
-    else
-    {
-        $requestUri = "$INCIDENT_URI`?&sysparm_exclude_reference_link=true&sysparm_display_value=true"
-    }
+        # build the requestUri
+        if(![String]::IsNullOrEmpty($incidentNumber))
+        {
+            $requestUri = "$INCIDENT_URI`?&sysparm_exclude_reference_link=true&sysparm_display_value=true&sysparm_query=number=$($incidentNumber.Trim())"
+        }
+        else
+        {
+            $requestUri = "$INCIDENT_URI`?&sysparm_exclude_reference_link=true&sysparm_display_value=true"
+        }
 
-    $fullUri = "$uri$requestUri"
+        $fullUri = "$uri$requestUri"
+        Write-Verbose "Full URI is $fullUri"
 
-    # send the request
-    Invoke-TableApiRequest -credential $credential -uri $fullUri -httpMethod Get
+        # send the request
+        Invoke-TableApiRequest -credential $credential -uri $fullUri -httpMethod Get
+    }
 }
 
 Function New-ServiceNowIncident
@@ -87,22 +93,26 @@ Function New-ServiceNowIncident
         [Parameter(Mandatory=$True)][string]$contactType
     )
 
-    # Build the URI
-    $requestUri = "$INCIDENT_URI`?&sysparm_exclude_reference_link=true&sysparm_display_value=true"
-    $fullUri = "$uri$requestUri"
+    PROCESS
+    {
+        # Build the URI
+        $requestUri = "$INCIDENT_URI`?&sysparm_exclude_reference_link=true&sysparm_display_value=true"
+        $fullUri = "$uri$requestUri"
+        Write-Verbose "Full URI is $fullUri"
 
-    # Crease HashTable for request body (content of the incident)
-    $requestHash = @{
-        'assignment_group' = $assignmentGroup;
-        'cmdb_ci' = $cmdbCI;
-        'category' = $category;
-        'short_description' = $shortDescription;
-        'comments' = $comments;
-        'contact_type' = $contactType;
-        }
+        # Crease HashTable for request body (content of the incident)
+        $requestHash = @{
+            'assignment_group' = $assignmentGroup;
+            'cmdb_ci' = $cmdbCI;
+            'category' = $category;
+            'short_description' = $shortDescription;
+            'comments' = $comments;
+            'contact_type' = $contactType;
+            }
 
-    # Send the request
-    Invoke-TableApiRequest -credential $credential -uri $fullUri -httpMethod Post -requestHash $requestHash
+        # Send the request
+        Invoke-TableApiRequest -credential $credential -uri $fullUri -httpMethod Post -requestHash $requestHash
+    }
 }
 
 Function Update-ServiceNowIncident
@@ -112,6 +122,10 @@ Function Update-ServiceNowIncident
         [Parameter(Mandatory=$True)][PSCredential]$credential,
         [Parameter(Mandatory=$True)][string]$uri
     )
+
+    PROCESS
+    {
+    }
 }
 
 Function Get-ServiceNowCI
@@ -121,6 +135,10 @@ Function Get-ServiceNowCI
         [Parameter(Mandatory=$True)][PSCredential]$credential,
         [Parameter(Mandatory=$True)][string]$uri
     )
+
+    PROCESS
+    {
+    }
 }
 
 Function Get-ServiceNowAssignmentGroup
@@ -130,6 +148,10 @@ Function Get-ServiceNowAssignmentGroup
         [Parameter(Mandatory=$True)][PSCredential]$credential,
         [Parameter(Mandatory=$True)][string]$uri
     )
+
+    PROCESS
+    {
+    }
 
 }
 
@@ -141,6 +163,7 @@ Function Get-ServiceNowUser
         [Parameter(Mandatory=$True)][string]$uri,
         [Parameter(Mandatory=$True)][string]$email
     )
+
     PROCESS
     {
         # build the requestUri
@@ -153,6 +176,7 @@ Function Get-ServiceNowUser
             $requestUri = "$USER_URI`?&sysparm_exclude_reference_link=true&sysparm_display_value=true"
         }
         $fullUri = "$uri$requestUri"
+        Write-Verbose "Full URI is $fullUri"
 
         # send the request
         Invoke-TableApiRequest -credential $credential -uri $fullUri -httpMethod Get
